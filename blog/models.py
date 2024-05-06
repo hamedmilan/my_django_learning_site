1d57533fe032c9e969ab19d141a86362c12e8800
Hamed Milanchian <hamed.milanchian@gmail.com>
2024-05-06 20:06:35 +0300

    sitemaps features added

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -30,5 +31,8 @@ class Post(models.Model):
     
     def snippet(self):
         return self.content[:100] + '...'
+    
+    def get_absolute_url(self):
+        return reverse('blog:single', kwargs={'pid':self.id})
     
 
